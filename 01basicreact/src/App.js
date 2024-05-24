b37d7260bc2d534f6e618ebdbfded040e415aa97
razzzi03 <162451631+razzzi03@users.noreply.github.com>
2024-05-24 21:49:16 +0500

    Simple project creation in react using create-react-app and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello world! || This is my first react program and i learned hoe to install react, and create react projects using simple method and vite. Also npm i is used to install missing files normally for node_modules when vite project is created.</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello world with vite</h1>
+  )
+}
+
+export default App
